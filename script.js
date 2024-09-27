a6a27578ab56a708df4830c52db793552c78c9fd
Karo_K <24210311+Karo-Kamil@users.noreply.github.com>
2024-09-27 23:45:43 +0300

    Add all the asked features
    This commit will add hover event that changes opacity and assigns a
    color to each square. There's also a reset button that clears the
    container div and lets you choose a new grid number.

--- script.js
+++ script.js
@@ -1,14 +1,60 @@
-container = document.querySelector(".container");
+container = document.querySelector("#container");
+btn = document.querySelector("button");
 divs = document.createElement("div");
 divs.classList.add("border");
 
 
+
+
+function getOpacity (element) {
+    const style = window.getComputedStyle(element);
+    return parseFloat(style.opacity);
+}
+
+
+
+function randomColor () {
+    const hex = "1234567890ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += hex.at(Math.floor(Math.random() * 16))
+    }
+    return color;
+}
+
+btn.addEventListener("click", () => {
+    let newRes = 0;
+    do {
+        newRes = prompt("If you continue this will create a new canvas with your desired resolution");
+    }
+    while (newRes < 1 || newRes > 100);
+    const content = document.querySelectorAll(".border");
+    container.replaceChildren();
+    fresh(+newRes);
+})
+
+container.addEventListener("mouseover", (event) =>{
+    let target = event.target;
+    console.log(target.class)
+    if (!(target.id === "container")){
+        let newOpacity = getOpacity(target) + 0.1;
+        target.style.opacity = newOpacity.toFixed(1);
+        if (!target.style.backgroundColor){
+            target.style.backgroundColor = randomColor();
+        }
+    }
+    
+})
+
+
 function fresh(num = 16) {
     let allSquares = num * num;
-    divs.style.width = `${960/num}px`;
-    divs.style.height = `${960/num}px`;
+    divs.style.width = `${Math.floor(960/num)}px`;
+    divs.style.height = `${Math.floor(960/num)}px`;
     for (let i = 0; i < allSquares; i++) {
         container.appendChild(divs.cloneNode(true));
     }
 }
+
+
 fresh();
